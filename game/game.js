3a0dbdaaea8e2714b40c8c7b78af8c7e54d13241
Matthias Jakobs <m.jakobs265@gmail.com>
2016-12-22 15:52:11 +0100

    added a level

--- game/game.js
+++ game/game.js
@@ -33,7 +33,7 @@ function setup() {
     createCanvas(dimensions.width*cellsize, dimensions.height*cellsize)
     p = inherit(player,entity)
     enemies = []
-    level = level1
+    level = level2
 
     spawnEnemies(level,5)
         .forEach( (enemy) => enemies.push(enemy) )
